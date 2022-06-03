709dd50d40fd043d782a9e9bb78b61a96b4b27b0
Rofeeyah <rofiatbalogun7@gmail.com>
2022-06-02 23:01:50 -0700

    5prnt

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+* main - Entry point for the C code
+*
+* Description: prints "program.."
+*
+* Return: 0 if success.
+*
+*/
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n") ;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0) ;
+	return (0);
 
 }
