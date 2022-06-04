0615badda349637e65688937b1d6ec2e2e52aeec
Rofeeyah <rofiatbalogun7@gmail.com>
2022-06-04 12:52:32 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the code C
+*
+* Description: prints the "program.."
+*
+* Return: Always (Success)
+*/
+
+int main(void)
+
+{
+
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+
+		putchar('\n');
+		return (0);
+
+}
